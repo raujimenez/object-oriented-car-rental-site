8137467aeb658ea29b8770ecf5b5c4c95c09130a
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-10-03 15:39:06 -0500

    added interface for view class

--- view.h
+++ view.h
@@ -1,9 +1,15 @@
 #ifndef __VIEW_H
 #define __VIEW_H
-
+#include "rental_site.h"
 class View
 {
-
-}
+    public:
+        View(Rental_Site rental_site);
+        std::string main_menu();
+        std::vector<Vehicle> list_of_vehicles();
+        std::string help();
+    private:
+        Rental_Site _rental_site;
+};
 
 #endif
