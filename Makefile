c55a7569a93e663ac957b4b762fd94d87f77ebce
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-10-03 19:29:41 -0500

    fixed regressiosn test for controller

--- Makefile
+++ Makefile
@@ -4,10 +4,10 @@ main: main.o controller.o view.o rental_site.o vehicle.o
 	${CXX} ${CXXFLAGS} -o main main.o controller.o view.o rental_site.o vehicle.o
 
 test: test_vehicle test_rental_site test_view test_controller
-	@./test_vehicle 2> errors_vehicle.txt
+	@./test_vehicle 2> errors_vehicle.txt 
 	@./test_rental_site 2> errors_rental_site.txt
 	@./test_view 2> errors_view.txt
-	@./test_controller 2> errors_controller.txt
+	@./test_controller 2> errors_controller.txt 1>expected.txt <test_controller_input.txt
 
 test_controller: test_controller.cpp controller.o view.o rental_site.o vehicle.o
 	${CXX} ${CXXFLAGS} -o test_controller test_controller.cpp controller.o view.o rental_site.o vehicle.o
@@ -30,4 +30,4 @@ vehicle.o: vehicle.cpp *.h
 	${CXX} ${CXXFLAGS} -c vehicle.cpp
 
 clean:
-	rm -rf errors_* *.o test_vehicle test_rental_site test_view test_controller main
+	rm -rf errors_* *.o test_vehicle test_rental_site test_view test_controller main expected.txt
--- test_controller.cpp
+++ test_controller.cpp
@@ -1,4 +1,5 @@
 #include "controller.h"
+#include <iostream>
 
 int main()
 {
@@ -11,32 +12,54 @@ int main()
     } 
     catch (std::exception& e)
     {
-        
+        std::cerr << e.what() << std::endl;
     }
     try
     {
-        cmd = 0; 
+        cmd = 1;
+        cont.execute_cmd(cmd);
     } 
     catch (std::exception& e)
     {
-
+        std::cerr << e.what() << std::endl;
     }
     try
     {
-
+        cmd = 2;
+        cont.execute_cmd(cmd);        
     } 
     catch (std::exception& e)
     {
-
+        std::cerr << e.what() << std::endl;
     }
     try
     {
-
+        cmd = 3;
+        cont.execute_cmd(cmd);
+        cont.execute_cmd(cmd);
     } 
     catch (std::exception& e)
     {
-
+        std::cerr << e.what() <<std::endl;
     }    
-
+    try
+    {
+        cmd = 4;
+        cont.execute_cmd(cmd);
+        cont.execute_cmd(cmd);
+    } 
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() <<std::endl;
+    }
+    try
+    {
+        cmd = 7;
+        cont.execute_cmd(cmd);
+    } 
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() <<std::endl;
+    }    
     return 0;
 }
